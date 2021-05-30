3ea5a3a1a7ca868e97b173765bd0b298a9c2634e
winebarrel <sugawara@winebarrel.jp>
2021-05-30 20:45:29 +0900

    update tab size

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 use std::io;
 
 fn main() -> io::Result<()> {
-  let opts = cli::parse_opts();
-  let f = fs::File::open(opts.file)?;
-  emsort::sort(f, io::stdout(), opts.capacity, None)
+    let opts = cli::parse_opts();
+    let f = fs::File::open(opts.file)?;
+    emsort::sort(f, io::stdout(), opts.capacity, None)
 }
