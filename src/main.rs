10e9aff906136bb85dddbaf02264a258c5fa740f
winebarrel <sugawara@winebarrel.jp>
2021-08-14 19:46:20 +0900

    Update ex_merge_sort

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ use std::io;
 fn main() -> io::Result<()> {
     let opts = cli::parse_opts();
     let f = fs::File::open(opts.file)?;
-    ex_merge_sort::sort(f, io::stdout(), opts.capacity, None)
+    ex_merge_sort::sort(f, io::stdout(), opts.capacity)
 }
